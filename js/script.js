616df55837c20c3df93e64cc246bd5c7c5438243
Polina Kyzylova <121396943+polina-kyzylova@users.noreply.github.com>
2023-07-06 17:24:15 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// открытие меню бургера
 $(document).ready(function() {
     $('.main_menu').click(function(){
         $('.burger, .main_menu').addClass('open-burger');
@@ -7,6 +8,7 @@ $(document).ready(function() {
 });
 
 
+// закрытие меню бургера по крестику
 $(document).ready(function() {
     $('.burger_menu').click(function(){
         $('.burger, .main_menu').addClass('close-burger');
@@ -14,3 +16,23 @@ $(document).ready(function() {
         $('body').removeClass('lock');
     });
 });
+
+
+// переход по ссылке для иконки VK
+$(function() {
+    $('.footer_vk, .burger_vk_logo').click(function() {
+        window.open('https://vk.com/objeika');
+    });
+});
+
+
+// закрытие меню бургера по клику вне него 
+window.addEventListener('click', e => { // при клике в любом месте окна браузера
+    const target = e.target // находим элемент, на котором был клик
+    if (!target.closest('.main_menu') && !target.closest('.burger_menu') && !target.closest('.burger')) { 
+        $('.burger, .main_menu').addClass('close-burger');
+        $('.burger, .main_menu').removeClass('open-burger');
+        $('body').removeClass('lock');
+    }
+  })
+
